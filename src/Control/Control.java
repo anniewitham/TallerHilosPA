bef0832489cfc95f3a0f4b2f3b7382035dced4be
Uvitaa <anamaria21qr@gmail.com>
2024-11-01 17:21:13 -0500

    parte ventana Carrera

---
+++ src/Control/Control.java
@@ -0,0 +1,16 @@
+
+package Control;
+
+import Modelo.Semaforo;
+import Vista.Carrera;
+import Vista.Nombres;
+
+public class Control {
+    private Carrera carrera;
+    private Semaforo semaforo;
+    private Carrera vistaCarrera;
+    private Nombres vistaNombres;
+    
+    
+
+}
