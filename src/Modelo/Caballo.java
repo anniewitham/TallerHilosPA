d7df7071fdfdb618e8583c9f9edde622745aed59
Patacon <samuelbarrerapulido@gmail.com>
2024-11-02 20:02:21 -0500

    modelo con javadoc

--- src/Modelo/Caballo.java
+++ src/Modelo/Caballo.java
@@ -1,9 +1,87 @@
-
 package Modelo;
 
+/**
+ * La clase Caballo representa un caballo en una carrera, con atributos como nombre, 
+ * posición actual, número de victorias y estado de interrupción.
+ * 
+ * @autor Ana Quientero, Juan Avila, Samuel Barrera
+ */
 public class Caballo {
     private String nombre;
     private int posicion;
     private int victorias;
     private boolean interrumpido;
+
+    /**
+     * Obtiene el nombre del caballo.
+     * 
+     * @return el nombre del caballo.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del caballo.
+     * 
+     * @param nombre el nombre a asignar al caballo.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obtiene la posición actual del caballo en la carrera.
+     * 
+     * @return la posición actual del caballo.
+     */
+    public int getPosicion() {
+        return posicion;
+    }
+
+    /**
+     * Establece la posición del caballo en la carrera.
+     * 
+     * @param posicion la posición a asignar al caballo.
+     */
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    /**
+     * Obtiene el número de victorias que ha conseguido el caballo.
+     * 
+     * @return el número de victorias del caballo.
+     */
+    public int getVictorias() {
+        return victorias;
+    }
+
+    /**
+     * Establece el número de victorias del caballo.
+     * 
+     * @param victorias el número de victorias a asignar al caballo.
+     */
+    public void setVictorias(int victorias) {
+        this.victorias = victorias;
+    }
+
+    /**
+     * Verifica si el caballo está interrumpido en la carrera.
+     * 
+     * @return true si el caballo está interrumpido, false en caso contrario.
+     */
+    public boolean isInterrumpido() {
+        return interrumpido;
+    }
+
+    /**
+     * Establece el estado de interrupción del caballo en la carrera.
+     * 
+     * @param interrumpido true para interrumpir al caballo, false en caso contrario.
+     */
+    public void setInterrumpido(boolean interrumpido) {
+        this.interrumpido = interrumpido;
+    }
 }
+
--- src/Modelo/Semaforo.java
+++ src/Modelo/Semaforo.java
@@ -1,6 +1,29 @@
-
 package Modelo;
 
+/**
+ * La clase Semaforo representa el semaforo que da inicio a la carrera.
+ * 
+ * @author Ana Quientero, Juan Avila, Samuel Barrera.
+ */
 public class Semaforo {
-     private String estado;
+    private String estado;
+
+    /**
+     * Obtiene el estado actual del semáforo.
+     * 
+     * @return el estado actual del semáforo, que puede ser, por ejemplo, "rojo", "verde" o "amarillo".
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * Establece el estado del semáforo.
+     * 
+     * @param estado el nuevo estado del semáforo, que puede ser "rojo", "verde" o "amarillo".
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
+
